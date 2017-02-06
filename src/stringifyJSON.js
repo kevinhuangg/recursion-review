1658ab119e9a210d46c206d02d305a8b361f7fdb
kevinhuangg <email.kevinh@gmail.com>
2017-02-06 14:54:52 -0800

    Implement stringifyJSON and make all tests pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,17 +5,38 @@
 
 var stringifyJSON = function(obj) {
   //if obj is a string, return string wrapped in double quotes
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
   
   //if obj is array, iterate array
-   //call stringifyJSON on each value
-   // return all stringified values, joined with ',' and wrapped in brackets
+  if (Array.isArray(obj)) {
+    //call stringifyJSON on each value
+    var stringifiedValues = [];
+    for (var i = 0; i < obj.length; i++) {
+      stringifiedValues.push(stringifyJSON(obj[i]));
+    }
+    // return all stringified values, joined with ',' and wrapped in brackets 
+    return '[' + stringifiedValues.join(',') + ']';
+  }
   
   //if obj is object, iterate over object
+  if (obj && typeof obj === 'object') {
     //declare result array
-    //call stringifyJSON on each property and key
-      //concat stringified key and property with ':' and push to result array
+    var result = [];
+    
+    for (var key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        //concat stringified key and property with ':' and push to result array
+        var pair = stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        result.push(pair);
+      }
+    }
     //return result array joined with ',' and wrapped in curly brackets
+    return '{' + result.join(',') + '}';
+  }
+  
 
   //otherwise
-    return '' + obj;
+  return '' + obj;
 };
