88789a18f0f17662768d3284b6c60f7955088af4
kevinhuangg <email.kevinh@gmail.com>
2017-02-06 16:32:46 -0800

    fixed test cases

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -6,9 +6,9 @@ describe('parseJSON', function() {
       var result = parseJSON(test);
       var expected = JSON.parse(test);
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
-      // Replace this line with an `expect` statement that tests
+      expect(equality).to.equal(true);
       // the behavior described by the `it` string
-      throw new Error('Test is missing.')
+      throw new Error('Test is missing.');
     });
   });
 
